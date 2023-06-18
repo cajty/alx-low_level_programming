d3438a47903ae0b7b6725e95187d50f62e2b5264
cajty <131008615+cajty@users.noreply.github.com>
2023-06-18 22:16:16 +0100

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry paint
+ *
+ * prints all alphabets using putchar
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+		putchar(a++);
+	while (A <= 'Z')
+		putchar(A++);
+	putchar('\n');
+	return (0);
+}
