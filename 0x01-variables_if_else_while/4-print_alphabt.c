325f0ae158352fd84b43c4f3994cda8af801017e
cajty <131008615+cajty@users.noreply.github.com>
2023-06-18 22:32:50 +0100

    Print all the letters except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry paint
+ *
+ * prints all alphabets using putchar
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'e' && a != 'q')
+		putchar(a++);
+	}
+	putchar('\n');
+	return (0);
+}
