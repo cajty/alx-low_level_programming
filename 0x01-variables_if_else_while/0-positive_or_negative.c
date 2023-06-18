837bd5513b82b0c02e9de5c48f4f105a5e3a700a
cajty <131008615+cajty@users.noreply.github.com>
2023-06-18 14:29:50 +0100

    The last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,12 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	srand(time0);
+	n = 
+		- RAND_MAX / 2;
 	if (n == 0)
 	{
-		printf("0 is zero\n");
+		:wprintf("0 is zero\n");
 	}
 	else if (n > 0)
 	{
