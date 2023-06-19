a7ae8b196e83efabc3c98a7a097ac50f58e0d068
cajty <131008615+cajty@users.noreply.github.com>
2023-06-19 01:25:25 +0100

    0-9 second

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0-9 using putchar
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (a <= 9)
+	{
+		putchar(c + '0');
+		c++;
+	}
+	putchar('\n');
+	return (0);
+
+}
