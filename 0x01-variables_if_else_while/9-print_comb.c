7bbb31632555a6d3a910032f713d134e0dbfe15b
cajty <131008615+cajty@users.noreply.github.com>
2023-06-20 01:18:44 +0100

    0*9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0-9 using putchar.
+ *
+ * Return: 0 (success)
+ */
+int	main(void)
+{
+	int a = 0;
+
+		while (a <= 9)
+		{
+			putchar(a + '0');
+			putchar(',');
+			putchar(' ');
+			a++;
+		}
+	return (0);
+}
