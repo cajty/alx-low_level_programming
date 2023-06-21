e37d60f36fb68733bd56b0c440ff2e44d7918a4d
cajty <131008615+cajty@users.noreply.github.com>
2023-06-22 00:38:11 +0100

    checks for alpahabtic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* _isalpha - Returns 1 if c is alphabetic Returns 0 otherwise.
+* _islapha - Returns 1 if c is alphabetic Returns 0 otherwise.
 * @c: input for function
 *
 * Return: Always 0.
 */
-int _isalpha(int c)
+int _islapha(int c)
 {
 	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
 		return (1);
