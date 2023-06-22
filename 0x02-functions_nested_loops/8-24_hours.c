ef03671e21b869746cded9e12a85da944d3559db
cajty <131008615+cajty@users.noreply.github.com>
2023-06-22 01:55:01 +0100

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59.
+ * Return: void.
+ */
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; y < 24; y++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			_putchar('0' + x / 10);
+			_putchar('0' + x % 10);
+			_putchar(':');
+			_putchar('0' + y / 10);
+			_putchar('0' + y % 10);
+			_putchar('\n');
+		}
+	}
+}
