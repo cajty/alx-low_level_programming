d3737c828fbab4c015bd268aad140791e5dbb8cb
cajty <131008615+cajty@users.noreply.github.com>
2023-06-22 01:47:27 +0100

    last ditgit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number.
+ *
+ * @n: integer input
+ * Return: i last digit.
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		a = (-1) * (n % 10);
+	else
+		a = n % 10;
+	_putchar(a + '0');
+	return (a);
+}
