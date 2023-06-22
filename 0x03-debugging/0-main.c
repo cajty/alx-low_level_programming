4834b3011492462c0f6566b1a7e0ec7d94ec6db5
cajty <131008615+cajty@users.noreply.github.com>
2023-06-22 17:53:08 +0100

    test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - test that function positive_or_negative with i=0
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
