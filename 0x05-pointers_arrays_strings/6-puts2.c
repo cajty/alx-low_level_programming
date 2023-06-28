3e239e382397f15b4162e1f741bbee9b0a18f7ae
cajty <131008615+cajty@users.noreply.github.com>
2023-06-28 01:26:10 +0100

    that prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * puts2 - prints a string other character of a string.
- *
- * @str: string to reverse.
+ * puts2 - prints every other character of a string.
+ * @str: string
  */
-
 void puts2(char *str)
 {
-	int c;
+	int c = 0;
 
 	while (str[c])
 	{
-	if (c % 2 == 0)
+		if (c % 2 == 0)
 			_putchar(str[c]);
 		c++;
 	}
