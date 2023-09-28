47c81b3b35df6df6526595e0ff8d938bb57c7ce9
cajty <131008615+cajty@users.noreply.github.com>
2023-09-28 19:05:47 +0100

    begin always with the true case

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Recursion is a method where the solution to a problemdepends on solutions to smaller instances of same problem
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recusion(char *s);
+int factorial(int n);
+int _pow_recusion(int x, int y);
+int _sqrt_recusion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int _putchar(char c);
+
+#endif /*MAIN_H*/
